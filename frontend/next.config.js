a83b8c383000d579c9a053eff8b2dd370d065624
Alexander DeRieux <alexander.derieux@gmail.com>
2022-09-14 19:28:55 -0400

    New Docker config to allow CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    // URI: 'your-docker-uri:port'
+    API_URI: `${process.env.API_SERVER_HOST}:${process.env.API_SERVER_PORT}`,
+    CANVAS_API_TOKEN: process.env.CANVAS_API_TOKEN,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // URI: 'http://localhost:port'
+    API_URI: `http://localhost:${process.env.API_SERVER_PORT}`,
+    CANVAS_API_TOKEN: process.env.CANVAS_API_TOKEN,
+  }
 }
 
 module.exports = nextConfig
