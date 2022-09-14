740860271b950d94b670c5c1df3223bbfdaefeb0
Alexander DeRieux <alexander.derieux@gmail.com>
2022-09-14 19:34:27 -0400

    New app layout
    - Uses Bootstrap from CDN
    - Uses new navbar components

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,33 @@
 // import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+
+import Head from "next/head";
+import Script from "next/script"
 import type { AppProps } from 'next/app'
+import Navbar from '../components/navbar';
+
+// Navbar elements.
+const NAVBAR_TITLE = "MDE Toolkit";
+const NAVBAR_MENU_LIST = [
+    { text: "Home", href: "/" },
+    { text: "Courses", href: "/courses" },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+    <>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    </Head>
+
+    <Navbar title={NAVBAR_TITLE} menu_list={NAVBAR_MENU_LIST}/>
+
+    <Component {...pageProps} />
+
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"/>
+    </>
+    );
 }
 
 export default MyApp
