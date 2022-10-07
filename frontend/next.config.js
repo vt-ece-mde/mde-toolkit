8d1d83d3043b76b916be59befde632a38d75a317
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-06 23:21:22 -0400

    Fix for API proxy in SSR calls

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,12 +7,27 @@ const nextConfig = {
     // URI: 'your-docker-uri:port'
     API_URI: `http://${process.env.API_SERVER_HOST}:${process.env.API_SERVER_PORT}`,
     CANVAS_API_TOKEN: process.env.CANVAS_API_TOKEN,
+    CANVAS_BASE_URL: "https://canvas.vt.edu/api/v1",
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
     // URI: 'http://localhost:port'
     API_URI: `http://localhost:${process.env.API_SERVER_PORT}`,
     CANVAS_API_TOKEN: process.env.CANVAS_API_TOKEN,
+    CANVAS_BASE_URL: "/api/canvas", // Intended to be empty.
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/mde/:path*',
+        // destination: 'http://localhost:3030/:path*' // Proxy to Backend
+        destination: `http://${process.env.API_SERVER_HOST}:${process.env.API_SERVER_PORT}/:path*` // Proxy to Backend
+      },
+      {
+        source: '/api/canvas/:path*',
+        destination: 'https://canvas.vt.edu/api/v1/:path*' // Proxy to Backend
+      }
+    ]
   }
 }
 
