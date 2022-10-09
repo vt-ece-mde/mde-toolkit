f730f6af43cef9d28b0529ddf73b9c6476da5fe3
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-09 17:13:08 -0400

    Refactored to call `useEffect()` and dynamic Canvas form inputs

--- frontend/pages/expo/team-dirs.tsx
+++ frontend/pages/expo/team-dirs.tsx
@@ -1,6 +1,8 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 import getConfig from 'next/config';
+import { CourseIdInput } from "../../components/dynamiccanvasform";
+import { fetch_courses } from "../../lib/canvas";
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 const API_URI = serverRuntimeConfig.API_URI || publicRuntimeConfig.API_URI;
 const CANVAS_API_TOKEN = serverRuntimeConfig.CANVAS_API_TOKEN || publicRuntimeConfig.CANVAS_API_TOKEN;
@@ -9,21 +11,39 @@ const CANVAS_API_TOKEN = serverRuntimeConfig.CANVAS_API_TOKEN || publicRuntimeCo
 
 
 
-type FormProps = {
-    course_id?: string;
-    onSubmit?(event: FormEvent): void;
+type FormData = {
+    course_id: number;
 }
+type FormProps = {
+    course_list: any[];
+    onSubmit(data: FormData): void;
+}
+function Form(props: FormProps) {
 
-function Form({ course_id, onSubmit }: FormProps) {
+    const [validForm, setValidForm] = useState<boolean>(false);
+    const [courseId, setCourseId] = useState<number>(-1);
+
+    useEffect(() => {
+        const valid = (courseId !== -1);
+        setValidForm(valid);
+    }, [courseId]);
+
+
+    /* Handle form submission. */
+    const onSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        const data: FormData = {
+            course_id: courseId,
+        }
+        props.onSubmit(data);
+    }
+
+
     return (
         <form className="needs-validation" onSubmit={ onSubmit }>
-        <div className="mb-3 mt-3">
-            <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">Course ID</span>
-                <input type="text" className="form-control" id="course_id" placeholder="Enter either a URL or an integer value" name="course_id" value={ course_id } />
-            </div>
-        </div>
-        <button type="submit" className="btn btn-success">Download Zip</button>
+        {/* Course ID selector. */}
+        <CourseIdInput course_list={props.course_list} onChange={setCourseId}/>
+        <button type="submit" className="btn btn-success" disabled={ !validForm }>Download Zip</button>
     </form>
     );
 }
@@ -32,55 +52,57 @@ function Form({ course_id, onSubmit }: FormProps) {
 export default function TeamDirs() {
 
     const [isFetching, setIsFetching] = useState(false);
+    const [courseList, setCourseList] = useState<any[]>([]);
+    const [formData, setFormData] = useState<FormData>();
 
-
-    const submitForm = async (event: FormEvent) => {
-        event.preventDefault();
-        setIsFetching(true);
-
-        // Re-cast the target as the appropriate type.
-        const target = event.target as HTMLFormElement
-        console.log(`course_id=${target.course_id.value}`);
-
-        // // Build date payload.
-        // const query_params = {
-        //     ...((target.course_id.value !== "") && {course_id: target.course_id.value}),
-        // }
-
-
-        fetch(`${API_URI}/courses/${target.course_id.value}/expo/team-dirs?type=zip`, {
-            headers: {Authorization: `Bearer ${CANVAS_API_TOKEN}`},
+    /* Get course list on initial mount (no fetch on re-render). */
+    useEffect(() => {
+        const getCourseList = async () => {
+            console.log(`Fetching courses...`)
+            var course_list = await (await fetch_courses()).filter(c => 'name' in c);
+            console.log(`Got courses: ${course_list.length}`)
+            setCourseList(course_list);
         }
-        ).then(
-            response => response.blob()
-        ).then(blob => {
-            const element = document.createElement("a");
-            element.href = URL.createObjectURL(blob);
-            element.download = "test.zip"; // Name of file.
-            document.body.appendChild(element); // Required for this to work in FireFox
-            element.click();
-            setIsFetching(false);
-        }).catch(error => {
-            console.log(error);
-        })
+        getCourseList();
+    }, []);
 
+    /* Fetch content based on form data. */
+    useEffect(() => {
+        const getContent = async (data: FormData) => {
+            setIsFetching(true); // Enable fetch state.
+    
+            fetch(`${API_URI}/courses/${data.course_id}/expo/team-dirs?type=zip`, {
+                headers: {Authorization: `Bearer ${CANVAS_API_TOKEN}`},
+            }
+            ).then(
+                response => response.blob()
+            ).then(blob => {
+                const element = document.createElement("a");
+                element.href = URL.createObjectURL(blob);
+                element.download = "test.zip"; // Name of file.
+                document.body.appendChild(element); // Required for this to work in FireFox
+                element.click();
+                setIsFetching(false);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
 
-        // // Make API call.
-        // const res = await fetch(`${API_URI}/courses/${target.course_id.value}/expo/team-dirs?type=zip`, {
-        //     headers: {Authorization: `Bearer ${CANVAS_API_TOKEN}`},
-        // })
-        // const courseList = await res.data;
-        // setIsFetching(false);
-    }
+        if (typeof formData !== 'undefined') {
+            getContent(formData);
+        }
+    }, [formData])
+
 
     if (isFetching === true) {
         return (<>
             <div className="container-sm p-5">
                 <h3>Filters</h3>
-                <Form onSubmit={ submitForm }/>
+                <Form course_list={ courseList } onSubmit={ setFormData } />
             </div>
-            <div className="container-fluid p-5">
+            <div className="d-flex align-items-center ml-5 mr-5">
                 <h3>Loading...</h3>
+                <div className="spinner-border ml-auto" role="status" aria-hidden="true"></div>
             </div>
         </>);
     }
@@ -89,7 +111,7 @@ export default function TeamDirs() {
         return (<>
             <div className="container-sm p-5">
                 <h3>Filters</h3>
-                <Form onSubmit={ submitForm }/>
+                <Form course_list={ courseList } onSubmit={ setFormData } />
             </div>
         </>);
     }
