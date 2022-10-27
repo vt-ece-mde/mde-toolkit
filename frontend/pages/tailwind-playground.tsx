66c98350514fa4ed9abd691da2a39f8cff9a049b
Alexander DeRieux <alexander.derieux@gmail.com>
2022-10-27 17:05:17 -0400

    Playing around with colors

--- frontend/pages/tailwind-playground.tsx
+++ frontend/pages/tailwind-playground.tsx
@@ -9,6 +9,11 @@ export default function TailwindPlayground() {
             <div>
             <div className="text-xl font-medium text-black">ChitChat</div>
             <p className="text-slate-500">You have a new message!</p>
+            <p className="text-[#83003F]">This uses a custom color "#83003F"</p>
+            <p className="text-blue-100">Color is "blue-100"</p>
+            <p className="text-blue-300">Color is "blue-300"</p>
+            <p className="text-blue-500">Color is "blue-500"</p>
+            <p className="text-blue-700">Color is "blue-700"</p>
             </div>
         </div>
     </>);
