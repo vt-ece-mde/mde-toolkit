3e96e2bf019de3c2318dc74d683a924d761bcd51
Alexander DeRieux <alexander.derieux@gmail.com>
2022-11-08 11:44:31 -0500

    Adds session provider wrapper

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,9 @@ import Script from "next/script"
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar';
 
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
 // Navbar elements.
 const NAVBAR_TITLE = "MDE Toolkit";
 const NAVBAR_MENU_LIST = [
@@ -16,20 +19,22 @@ const NAVBAR_MENU_LIST = [
     { text: "Expo Team Dirs", href: "/expo/team-dirs" },
 ];
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps<{session: Session}>) {
     return (
     <>
-    <Head>
-        <title>MDE Toolkit</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    </Head>
+    <SessionProvider session={pageProps.session}>
+        <Head>
+            <title>MDE Toolkit</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        </Head>
 
-    <Navbar title={NAVBAR_TITLE} menu_list={NAVBAR_MENU_LIST}/>
+        <Navbar title={NAVBAR_TITLE} menu_list={NAVBAR_MENU_LIST}/>
 
-    <Component {...pageProps} />
+        <Component {...pageProps} />
 
-    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"/>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"/>
+    </SessionProvider>
     </>
     );
 }
