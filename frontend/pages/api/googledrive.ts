d744a8bf83e5b89ec680451c1b237093293f8cd4
Alexander DeRieux <alexander.derieux@gmail.com>
2022-11-09 15:10:28 -0500

    WIP building team drive traversal

--- frontend/pages/api/googledrive.ts
+++ frontend/pages/api/googledrive.ts
@@ -4,7 +4,7 @@ import { unstable_getServerSession } from "next-auth/next"
 import { NextAuthOptions } from "next-auth"
 import { authOptions } from "./auth/[...nextauth]"
 
-import { google } from "googleapis";
+import { drive_v3, google } from "googleapis";
 // import drive from "@googleapis/drive";
 
 import type { AuthSession } from '../../lib/auth';
@@ -27,11 +27,55 @@ const scopes = [
 // }
 
 
+async function processTeamFolder(client: drive_v3.Drive, folderId: string): Promise<drive_v3.Schema$File[]> {
+
+    const mimeTypes = [
+        'text/plain',
+    ]
+    const res = await client.files.list({
+        supportsAllDrives: true,
+        includeItemsFromAllDrives: true,
+        // q: `'${folderId}' in parents and (mimeType = 'text/plain' or )`,
+        q: `'${folderId}' in parents and mimeType != 'application/vnd.google-apps.folder'`,
+    })
+    const files = res.data.files;
+    if (files?.length) {
+        return files;
+    } else {
+        return [];
+    }
+
+}
+
+async function processExpoTeamRoot(client: drive_v3.Drive, folderId: string): Promise<drive_v3.Schema$File[]> {
+    const res = await client.files.list({
+        supportsAllDrives: true,
+        includeItemsFromAllDrives: true,
+        q: `'${folderId}' in parents and mimeType = 'application/vnd.google-apps.folder'`,
+    })
+    const files = res.data.files;
+    if (files?.length) {
+        return files;
+    } else {
+        return [];
+    }
+}
+
+
+
 // https://github.com/nextauthjs/next-auth/issues/1162#issuecomment-766331341
+
+interface TeamBlob {
+    team_root: drive_v3.Schema$File;
+    team_files: drive_v3.Schema$File[];
+}
+interface Data {
+    teams: TeamBlob[];
+}
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse//<Data>
+    res: NextApiResponse<Data>
 ) {
 
     const session: AuthSession|null = await unstable_getServerSession(req, res, (authOptions as NextAuthOptions))
@@ -71,23 +115,49 @@ export default async function handler(
 
     const client = google.drive({ auth, version: "v3" });
 
-    try {
-        // const dres = await drive.files.list({ spaces: "appDataFolder" })
-        const dres = await client.files.list()
-        const files = dres.data.files;
-        if (files?.length) {
-            files.map((file) => {
-                console.log(`FILE: ${file.name}`);
-            });
-            res.status(200).json(files)
-        } else {
-            console.log('No files found');
-            res.status(401)
+    const folder_id = "1D4SAg0_t_s9vrkiwSJEoWZFF_OiG-G29";
+
+    const teamFolders = await processExpoTeamRoot(client, folder_id)
+
+    var teams: TeamBlob[] = []
+    for (let index = 0; index < teamFolders.length; index++) {
+        const team_root = teamFolders[index];
+        if (team_root.id) {
+            const team_files = await processTeamFolder(client, team_root.id)
+            teams.push({
+                team_root: team_root,
+                team_files: team_files,
+            })
         }
-    } catch (e) {
-        console.log(e)
-        res.status(500).json(e);
+        // break;
     }
+
+    res.status(200).json({teams})
+
+
+    // try {
+    //     // const dres = await drive.files.list({ spaces: "appDataFolder" })
+    //     const folder_id = "1D4SAg0_t_s9vrkiwSJEoWZFF_OiG-G29"
+    //     // const folder_id = "0B3orFCWMG2hoZnlpdFppdV90LUE"
+    //     const dres = await client.files.list({
+    //         supportsAllDrives: true,
+    //         includeItemsFromAllDrives: true,
+    //         q: `'${folder_id}' in parents`,
+    //     })
+    //     const files = dres.data.files;
+    //     if (files?.length) {
+    //         files.map((file) => {
+    //             console.log(`FILE: ${file.name}`);
+    //         });
+    //         res.status(200).json(files)
+    //     } else {
+    //         console.log('No files found');
+    //         res.status(401)
+    //     }
+    // } catch (e) {
+    //     console.log(e)
+    //     res.status(500).json(e);
+    // }
 
     // res.status(200).json({ name: 'John Doe' })
 }
