0e66a537ceff7622177023880b416041d34642ea
Alexander DeRieux <alexander.derieux@gmail.com>
2022-11-08 22:24:40 -0500

    Buttons now use Tailwind styles

--- frontend/pages/ipr-history-spreadsheet.tsx
+++ frontend/pages/ipr-history-spreadsheet.tsx
@@ -120,12 +120,12 @@ function Form(props: FormProps) {
                 </select>
                 {
                     (assignmentIdList.length > 1) ? (
-                        <button className="btn btn-danger" onClick={() => setAssignmentIdList(prev => prev.filter((_,i) => i !== index))}>delete</button>
+                        <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={() => setAssignmentIdList(prev => prev.filter((_,i) => i !== index))}>delete</button>
                     ) : null
                 }
                 {
                     index === assignmentIdList.length-1 ? (
-                        <button className="btn btn-success" onClick={() => setAssignmentIdList(prev => [...prev, -1])}>add</button>
+                        <button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" onClick={() => setAssignmentIdList(prev => [...prev, -1])}>add</button>
                     ) : null
                 }
             </div>
@@ -158,7 +158,7 @@ function Form(props: FormProps) {
                 {/* Render assignment ID drop-down boxes. */}
                 {renderAssignmentIdInputElement()}
             </div>
-            <button type="submit" className="btn btn-primary" disabled={ !validForm }>Create Spreadsheet</button>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled={ !validForm }>Create Spreadsheet</button>
         </form>
     );
 }
