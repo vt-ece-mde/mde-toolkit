47aca11c777faf2f2a2ed369634af1d189da71b6
Alexander DeRieux <alexander.derieux@gmail.com>
2022-11-08 23:36:53 -0500

    Adds Google auth token to session via jwt

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,16 +1,33 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+
+const scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/drive",
+];
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            // authorizationUrl: "https://accounts.google.com/o/oauth2/auth?response_type=code&hd=vt.edu", // hosted domain is vt.edu
+            scope: scopes.join(" "),
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
+    jwt: {
+        encryption: true,
+        secret: process.env.NEXTAUTH_SECRET,
+    },
     pages: {
         signIn: '/auth/signin',
         error: '/auth/error',
@@ -22,5 +39,23 @@ export default NextAuth({
             }
             return true
         },
+        async jwt({ token, user, account, profile, isNewUser }) {
+
+            // Add access_token to the token right after signin.
+            if (account?.access_token) {
+                token.access_token = account.access_token
+            }
+            // Add refresh_token to the token right after signin.
+            if (account?.refresh_token) {
+                token.refresh_token = account.refresh_token
+            }
+            return token;
+          },
+        async session({ session, token, user }) {
+            // Add properties to session, like an access_token from a provider.
+            session.access_token = token.access_token
+            session.refresh_token = token.refresh_token
+            return session
+        }
     },
 })
