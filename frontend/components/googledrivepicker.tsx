ab1e224c9728a6a731c97fa9f05a84e735ddfc20
Alexander DeRieux <alexander.derieux@gmail.com>
2022-11-14 19:43:25 -0500

    Adds support for Google Drive picker
    A "hack" is required to make this work, which overrides the default behavior of the NPM package `react-google-drive-picker`

---
+++ frontend/components/googledrivepicker.tsx
@@ -0,0 +1,199 @@
+/**
+ * DISCLAIMER
+ * 
+ * This entire file is a workaround for the NPM repository `react-google-drive-picker`.
+ * https://www.npmjs.com/package/react-google-drive-picker
+ * 
+ * The problem is that the default behavior of the tool requires a Google Developer API key,
+ * which is not technically required if a post-auth access token is present. Therefore, to bypass this,
+ * a totally separate implementation is required which simply removes the call to set the
+ * developer key.
+ * 
+ * Search for the tag "CHANGEME" to see which lines were changed.
+ * 
+ * This code was gleaned directly from the source repository and we in no way assume ownership
+ * of the original design. 
+ * 
+ * For original code, see: https://github.com/Jose-cd/React-google-drive-picker/blob/master/src/index.tsx
+ */
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let google: any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let window: any
+import { useEffect, useState } from 'react'
+import {
+  authResult,
+  defaultConfiguration,
+  PickerConfiguration,
+} from 'react-google-drive-picker/dist/typeDefs'
+import useInjectScript from 'react-google-drive-picker/dist/useInjectScript'
+
+// export declare type PickerConfiguration = PickerConfiguration & {
+//     clientId?: string;
+//     developerKey?: string;
+// };
+// export declare const newDefaultConfiguration: PickerConfiguration;
+// export {};
+
+export default function useDrivePicker(): [
+  (config: PickerConfiguration) => boolean | undefined,
+  authResult | undefined
+] {
+  const defaultScopes = ['https://www.googleapis.com/auth/drive.readonly']
+  const [loaded, error] = useInjectScript('https://apis.google.com/js/api.js')
+  const [loadedGsi, errorGsi] = useInjectScript(
+    'https://accounts.google.com/gsi/client'
+  )
+  const [pickerApiLoaded, setpickerApiLoaded] = useState(false)
+  const [openAfterAuth, setOpenAfterAuth] = useState(false)
+  const [authWindowVisible, setAuthWindowVisible] = useState(false)
+  const [config, setConfig] =
+    useState<PickerConfiguration>(defaultConfiguration)
+  const [authRes, setAuthRes] = useState<authResult>()
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let picker: any
+
+  // get the apis from googleapis
+  useEffect(() => {
+    if (loaded && !error && loadedGsi && !errorGsi && !pickerApiLoaded) {
+      loadApis()
+    }
+  }, [loaded, error, loadedGsi, errorGsi, pickerApiLoaded])
+
+  // use effect to open picker after auth
+  useEffect(() => {
+    if (
+      openAfterAuth &&
+      config.token &&
+      loaded &&
+      !error &&
+      loadedGsi &&
+      !errorGsi &&
+      pickerApiLoaded
+    ) {
+      createPicker(config)
+      setOpenAfterAuth(false)
+    }
+  }, [
+    openAfterAuth,
+    config.token,
+    loaded,
+    error,
+    loadedGsi,
+    errorGsi,
+    pickerApiLoaded,
+  ])
+
+  // open the picker
+  const openPicker = (config: PickerConfiguration) => {
+    // global scope given conf
+    setConfig(config)
+
+    // if we didnt get token generate token.
+    if (!config.token) {
+      const client = google.accounts.oauth2.initTokenClient({
+        client_id: config.clientId,
+        scope: (config.customScopes
+          ? [...defaultScopes, ...config.customScopes]
+          : defaultScopes
+        ).join(' '),
+        callback: (tokenResponse: authResult) => {
+          setAuthRes(tokenResponse)
+          createPicker({ ...config, token: tokenResponse.access_token })
+        },
+      })
+
+      client.requestAccessToken()
+    }
+
+    // if we have token and everything is loaded open the picker
+    if (config.token && loaded && !error && pickerApiLoaded) {
+      return createPicker(config)
+    }
+  }
+
+  // load the Drive picker api
+  const loadApis = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window.gapi.load('auth')
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window.gapi.load('picker', { callback: onPickerApiLoad })
+  }
+
+  const onPickerApiLoad = () => {
+    setpickerApiLoaded(true)
+  }
+
+  const createPicker = ({
+    token,
+    appId = '',
+    supportDrives = false,
+    developerKey = '',
+    viewId = 'DOCS',
+    disabled,
+    multiselect,
+    showUploadView = false,
+    showUploadFolders,
+    setParentFolder = '',
+    viewMimeTypes,
+    customViews,
+    locale = 'en',
+    setIncludeFolders,
+    setSelectFolderEnabled,
+    disableDefaultView = false,
+    callbackFunction,
+  }: PickerConfiguration) => {
+    if (disabled) return false
+
+    const view = new google.picker.DocsView(google.picker.ViewId[viewId])
+    if (viewMimeTypes) view.setMimeTypes(viewMimeTypes)
+    if (setIncludeFolders) view.setSelectFolderEnabled(true)
+    if (setSelectFolderEnabled) view.setSelectFolderEnabled(true)
+
+    const uploadView = new google.picker.DocsUploadView()
+    if (viewMimeTypes) uploadView.setMimeTypes(viewMimeTypes)
+    if (showUploadFolders) uploadView.setIncludeFolders(true)
+    if (setParentFolder) uploadView.setParent(setParentFolder)
+
+    picker = new google.picker.PickerBuilder()
+      .setAppId(appId)
+      .setOAuthToken(token)
+    //   .setDeveloperKey(developerKey) // CHANGEME: This is not required if a token is provided.
+      .setLocale(locale)
+      .setCallback(callbackFunction)
+
+    if (!disableDefaultView) {
+      picker.addView(view)
+    }
+
+    if (customViews) {
+      customViews.map((view) => picker.addView(view))
+    }
+
+    if (multiselect) {
+      picker.enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+    }
+
+    if (showUploadView) picker.addView(uploadView)
+
+    // CHANGEME: Adds new "Shared drives" folder view.
+    const folderView = new google.picker.DocsView(google.picker.ViewId[viewId])
+        .setIncludeFolders(true)
+        .setEnableDrives(true)
+        .setSelectFolderEnabled(true);
+    picker.addView(folderView);
+
+    if (supportDrives) {
+      picker.enableFeature(google.picker.Feature.SUPPORT_DRIVES)
+    }
+
+    picker.build().setVisible(true)
+
+    return true
+  }
+
+  return [openPicker, authRes]
+}
--- frontend/pages/googledrive.tsx
+++ frontend/pages/googledrive.tsx
@@ -1,73 +1,185 @@
 import { getSession } from 'next-auth/react'
 import { GetServerSidePropsContext } from 'next';
 import { google } from "googleapis";
-import type { AuthSession } from '../lib/auth';
+// import drive from "@googleapis/drive"
 
-// const clientId = process.env.GOOGLE_CLIENT_ID;
-// const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+import  { useEffect } from 'react';
+// import useDrivePicker from 'react-google-drive-picker'
+import useDrivePicker from '../components/googledrivepicker'
+import { Session } from 'next-auth';
+
+
+// import GooglePicker from 'react-google-picker';
+
+
 
 // const scopes = [
 //     'https://www.googleapis.com/auth/drive',
 // ];
 
 
-// export default function GoogleDrivePage({ session }: { session: AuthSession }) {
-
-//     const access_token = session?.access_token;
-//     const refresh_token = session?.refresh_token;
+export default function GoogleDrivePage({ session }: { session: Session }) {
+// export default function GoogleDrivePage({ drive }: { drive: drive_v3.Drive }) {
 
 
-//     const readDrive = async () => {
-//         const auth = new google.auth.JWT({
-//             email: session.user?.email || '',
-//             key: session.access_token,
-//             scopes: scopes,
-//         })
-//         // const auth = new google.auth.OAuth2({
-//         //     clientId,
-//         //     clientSecret,
-//         // });
-//         auth.setCredentials({
-//             access_token: access_token,
-//             refresh_token: refresh_token,
-//         });
 
-//         const drive = google.drive({ auth, version: "v3" });
+    const access_token = session?.access_token;
+    const refresh_token = session?.refresh_token;
 
-//         const res = await drive.files.list({})
-//         const files = res.data.files;
-//         if (files?.length) {
-//             files.map((file) => {
-//                 console.log(`FILE: ${file}`);
-//             });
-//         } else {
-//             console.log('No files found');
-//         }
-//     }
+    const [openPicker, authResponse] = useDrivePicker();  
+    // const customViewsArray = [new google.picker.DocsView()]; // custom view
+    const handleOpenPicker = () => {
+        openPicker({
+            clientId: "", // Not required, but must be provided as an empty string.
+            developerKey: "", // Not required, but must be provided as an empty string.
+            token: access_token, // pass oauth token in case you already have one
+            viewId: "DOCS", // All Google Drive document types.
+            showUploadView: false,
+            showUploadFolders: true,
+            supportDrives: true,
+            multiselect: true,
+            customViews: [
 
-//     return (<>
-//         <button onClick={ _ => readDrive() }>Read Drive</button>
-//     </>);
-// }
+            ],
+            callbackFunction: (data) => {
+            if (data.action === 'cancel') {
+                console.log('User clicked cancel/close button')
+            } else {
+                console.log(`data? ${JSON.stringify(data)}`)
+            }
+            console.log(data)
+            },
+        })
+    }
 
-// export async function getServerSideProps(context: GetServerSidePropsContext) {
+    // function createPicker() {
+    //     var picker = new google.picker.PickerBuilder()
+    //     .addView(new google.picker.DocsView())
+    //     .setOAuthToken(access_token)
+    //     // .setCallback(pickerCallback)
+    //     .build();
+    //     picker.setVisible(true);
+    // }
 
-//     const { req } = context;
-//     const session = await getSession({ req });
 
-//     console.log(`session? ${JSON.stringify(session)}`)
+    // const readDrive = async () => {
+    //     // const auth = new google.auth.OAuth2({
+    //     //     clientId,
+    //     //     clientSecret,
+    //     //     // redirectUri
+    //     // });
+    //     const auth = new drive.auth.OAuth2();
+    //     auth.setCredentials({
+    //         access_token: access_token,
+    //         refresh_token: refresh_token,
+    //         // scope: scopes.join(" "),
+    //     });
 
-//     // Redirect to signin page.
-//     if (!session) {
-//         return {
-//             redirect: { destination: '/auth/signin', permanent: false },
-//         }
-//     }
+    //     const client = drive.drive({ auth, version: "v3" });
 
-//     // Render desired page with session.
-//     return {
-//         props: { 
-//             session,
-//         },
-//     }
-// }
+    //     const res = await client.files.list({})
+    //     const files = res.data.files;
+    //     if (files?.length) {
+    //         files.map((file) => {
+    //             console.log(`FILE: ${file}`);
+    //         });
+    //     } else {
+    //         console.log('No files found');
+    //     }
+    // }
+
+    return (<>
+        {/* <div>hello</div> */}
+        <button onClick={ _ => handleOpenPicker() }>Read Drive</button>
+    </>);
+
+
+
+    // return (<>
+    // <GooglePicker clientId={'xxxx'}
+    //           developerKey={"xxxx"}
+    //           scope={['https://www.googleapis.com/auth/drive.readonly']}
+    //           onChange={data => console.log('on change:', data)}
+    //           onAuthenticate={token => console.log('oauth token:', token)}
+    //           onAuthFailed={data => console.log('on auth failed:', data)}
+    //           multiselect={true}
+    //           navHidden={true}
+    //           authImmediate={false}
+    //           viewId={'FOLDERS'}
+    //           createPicker={ (google, oauthToken) => {
+    //             const googleViewId = google.picker.ViewId.FOLDERS;
+    //             const docsView = new google.picker.DocsView(googleViewId)
+    //                 .setIncludeFolders(true)
+    //                 .setMimeTypes('application/vnd.google-apps.folder')
+    //                 .setSelectFolderEnabled(true)
+    //                 .setEnableDrives(true);
+
+    //             const picker = new window.google.picker.PickerBuilder()
+    //                 .addView(docsView)
+    //                 .enableFeature(window.google.picker.Feature.SUPPORT_DRIVES)
+    //                 .setOAuthToken(access_token)
+    //                 // .setDeveloperKey(DEVELOPER_KEY)
+    //                 .setCallback(()=>{
+    //                   console.log('Custom picker is ready!');
+    //                 })
+    //                 .build().setVisible(true);
+
+    //             enableFeature(google.picker.Feature.MULTISELECT_ENABLED) // added code
+    //         }}
+    //     >
+    //         <button>Click</button>
+    //     </GooglePicker>
+    // </>);
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+    const { req, resolvedUrl } = context;
+    const session = await getSession({ req });
+
+    // console.log(`session? ${JSON.stringify(session)}`)
+
+    // Redirect to signin page.
+    if (!session || session?.error === "RefreshAccessTokenError") {
+        return {
+            redirect: { 
+                destination: '/auth/signin?' + new URLSearchParams({
+                    redirect: resolvedUrl,
+                }),
+                permanent: false,
+            },
+        }
+    }
+
+    // const clientId = process.env.GOOGLE_CLIENT_ID;
+    // const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    // const access_token = session?.access_token;
+    // const refresh_token = session?.refresh_token;
+    // const auth = new google.auth.OAuth2({
+    //     clientId,
+    //     clientSecret,
+    //     // redirectUri
+    // });
+    // auth.setCredentials({
+    //     access_token: access_token,
+    //     refresh_token: refresh_token,
+    //     // scope: scopes.join(" "),
+    // });
+
+    // const drive = google.drive({ auth, version: "v3" });
+
+    // Render desired page with session.
+    return {
+        props: { 
+            session,
+            // drive,
+        },
+    }
+}
+
+
+
+
+const newUsePicker = (access_token: string) => {
+
+}
