2c886ca69003077ffb4de0a144f5d9b900272942
Alexander DeRieux <alexander.derieux@gmail.com>
2022-12-10 16:04:24 -0500

    Fixes bug with teams list not being reset after new picker selection

--- frontend/pages/expo/team-brochure.tsx
+++ frontend/pages/expo/team-brochure.tsx
@@ -395,6 +395,9 @@ export default function TeamBrochurePage({ session }: { session: Session }) {
 
             dispatch({ type: 'update-state', state: {
                 fetching: true,
+                parentId: '',
+                teams: new Map<string, ParsedTeam>(),
+                selectedTeamToDisplay: '',
             }});
 
             // Get ID of parent folder to make subsequent Drive picker calls easier.
@@ -599,7 +602,7 @@ export default function TeamBrochurePage({ session }: { session: Session }) {
         })
     }
 
-
+    
     return (<>
         <div className='flex flex-col items-center justify-center pt-4'>
 
@@ -771,13 +774,13 @@ export default function TeamBrochurePage({ session }: { session: Session }) {
                                         uploadTeamToGoogleDrive(id, team!, root!);
                                     }
                                 });
-                            } } disabled={fetching}>
+                            } } disabled={fetching || Array.from(teams.entries()).every(([id, pt]) => pt.status.ok === undefined)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 -ml-1 mr-2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                                 </svg>
                                 Upload all to Google Drive
                             </button>
-                            <button className="bg-purple-500 hover:bg-purple-700 disabled:bg-slate-400 disabled:text-slate-100 text-white font-bold py-2 px-4 rounded flex flex-row" onClick={ () => downloadTeamListAsHTML(Array.from(teams.values()).filter((pt) => pt.status.ok !== undefined).map((pt) => pt.team!)) } disabled={fetching}>
+                            <button className="bg-purple-500 hover:bg-purple-700 disabled:bg-slate-400 disabled:text-slate-100 text-white font-bold py-2 px-4 rounded flex flex-row" onClick={ () => downloadTeamListAsHTML(Array.from(teams.values()).filter((pt) => pt.status.ok !== undefined).map((pt) => pt.team!)) } disabled={fetching || Array.from(teams.entries()).every(([id, pt]) => pt.status.ok === undefined)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 -ml-1 mr-2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                 </svg>
