3a22f58c10905d2e48fe748ef710769469da7b97
Alexander DeRieux <alexander.derieux@gmail.com>
2022-12-14 17:24:51 -0500

    Adds download buttons for presentation and poster

--- frontend/components/TeamBrochure/TeamBrochure.tsx
+++ frontend/components/TeamBrochure/TeamBrochure.tsx
@@ -257,7 +257,24 @@ export default function TeamBrochure( props: Team ) {
             <div className='pt-5'>
                 <div className='flex flex-row space-x-8'>
                     <div className='flex flex-col w-1/2 h-[600px] space-y-2'>
-                        <div className='text-4xl font-bold text-center text-[#231F20]'>Poster</div>
+                        <div className='flex flex-row items-center justify-center'>
+                            <div className='text-4xl font-bold text-center text-[#231F20]'>Poster</div>
+                            {props.posterUrl
+                                ? (
+                                    <div className='pl-2'>
+                                        <a className='flex flex-row group relative' href={props.posterUrl} target="_blank" rel="noopener noreferrer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                            </svg>
+                                            <span className="items-center justify-center absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-36 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/4 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700 hover:invisible">
+                                                Download File
+                                            </span>
+                                        </a>
+                                    </div>
+                                )
+                                : (null)
+                            }
+                        </div>
                         <EmbedFileUrl url={props.posterUrl}>
                             <div className='flex flex-col text-center justify-center bg-slate-100 w-[100%] h-[100%]'>
                                 {props.posterUrl 
@@ -271,7 +288,24 @@ export default function TeamBrochure( props: Team ) {
                         </EmbedFileUrl>
                     </div>
                     <div className='flex flex-col w-1/2 h-[600px] space-y-2'>
-                        <div className='text-4xl font-bold text-center text-[#231F20]'>Presentation</div>
+                        <div className='flex flex-row items-center justify-center'>
+                            <div className='text-4xl font-bold text-center text-[#231F20]'>Presentation</div>
+                            {props.presentationUrl
+                                ? (
+                                    <div className='pl-2'>
+                                        <a className='flex flex-row group relative' href={props.presentationUrl} target="_blank" rel="noopener noreferrer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                            </svg>
+                                            <span className="items-center justify-center absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-36 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/4 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700 hover:invisible">
+                                                Download File
+                                            </span>
+                                        </a>
+                                    </div>
+                                )
+                                : (null)
+                            }
+                        </div>
                         <EmbedFileUrl url={props.presentationUrl}>
                             <div className='flex flex-col text-center justify-center bg-slate-100 w-[100%] h-[100%]'>
                                 {props.presentationUrl 
