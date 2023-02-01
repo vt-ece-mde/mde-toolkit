8d570e51326d2c44e3af6d01c9e4fc37eaa25ddd
Alexander DeRieux <alexander.derieux@gmail.com>
2023-02-01 11:01:37 -0500

    Adds alternate description

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -97,7 +97,7 @@ function UserDropdown({ session }: UserDropDownProps) {
                     <Menu.Button>
                         <div>
                             {session.user?.image ? (
-                                <Image src={session.user?.image} className="rounded-full" width={50} height={50} title={ `Logged into Google as: ${session.user?.name}` }/>
+                                <Image alt="User Icon" src={session.user?.image} className="rounded-full" width={50} height={50} title={ `Logged into Google as: ${session.user?.name}` }/>
                             ) : (
                                 <div className='rounded-full w-12 h-12'>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
